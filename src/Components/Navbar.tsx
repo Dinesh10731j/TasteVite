795b06bb72b6a55607a934446e1187903ca15be8
Dinesh10731j <dinesh1122@gmail.com>
2024-06-23 00:52:58 +0545

    Responsive added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     
     <nav className="flex fex-row justify-evenly items-center  py-4 ">
 <h1 className="font-serif text-4xl font-medium tracking-wide">TasteVite</h1>
-<ul className="flex flex-col gap-5 md:flex-row font-sans font-medium text-2xl">
+<ul className="flex flex-col gap-5 m font-sans bg-slate-300 px-3 py-2 font-medium text-2xl fixed right-0 top-20 md:flex-row md:relative md:top-0  ">
     <li>Home</li>
     <li>Find Receipe</li>
     <li>Buy</li>
 </ul>
-<button className="px-5 py-2 t bg-cyan-500 rounded-md font-medium">Login</button>
-<button className="px-5 py-2 bg-cyan-500 rounded-md font-medium">Signup</button>
+<button className="px-2 py-2 t bg-cyan-500 rounded-md font-medium md:px-3">Login</button>
+<button className="px-2 py-2 bg-cyan-500 rounded-md font-medium md:px-3" >Signup</button>
 <svg className="block md:hidden" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
   <rect y="4" width="24" height="2" fill="#000"/>
   <rect y="11" width="24" height="2" fill="#000"/>
