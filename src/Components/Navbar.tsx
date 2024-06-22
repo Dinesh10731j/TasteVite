f4472cf3314418c0625f180fcf0d431b2c9418fb
Dinesh10731j <dinesh1122@gmail.com>
2024-06-23 01:09:27 +0545

    responsive added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 </ul>
 <button className="px-2 py-2 t bg-cyan-500 rounded-md font-medium md:px-3">Login</button>
 <button className="px-2 py-2 bg-cyan-500 rounded-md font-medium md:px-3" >Signup</button>
-<svg className="block md:hidden" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<svg className="block cursor-pointer md:hidden" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
   <rect y="4" width="24" height="2" fill="#000"/>
   <rect y="11" width="24" height="2" fill="#000"/>
   <rect y="18" width="24" height="2" fill="#000"/>
