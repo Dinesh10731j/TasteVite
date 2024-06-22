c77f950aa4b5694a89a03422928174409b104daa
Dinesh10731j <dinesh1122@gmail.com>
2024-06-23 00:36:02 +0545

    additional features added

--- src/Searchbar/Searchbar.tsx
+++ src/Searchbar/Searchbar.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from "react-query";
 import { axiosInstance } from "../api/BaseUrl";
-import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { ThreeCircles } from "react-loader-spinner";
 
 interface ResponseData {
   idMeal: string;
@@ -15,32 +15,28 @@ interface Types {
   Receipename: string;
 }
 
-
-
 const Searchbar = () => {
+  const searchRecipe = async (): Promise<ResponseData[]> => {
+    try {
+      const response = await axiosInstance.get(`?s=${receipename}`);
 
-    const searchRecipe = async (): Promise<ResponseData[]> => {
-        try {
-          const response = await axiosInstance.get(`?s=${receipename}`);
-    
-      toast.success("Meal Load Successfully")
-          if (response.data && Array.isArray(response.data.meals)) {
-            return response.data.meals;
-          } else {
-            throw new Error('API response does not contain meals array');
-            toast.error
-          }
-        } catch (error) {
-
-          throw error; // Rethrow the error so useQuery can handle it
-        }
+      if (response.data && Array.isArray(response.data.meals)) {
+        return response.data.meals;
+      } else {
+        throw new Error("API response does not contain meals array");
       }
-    
+    } catch (error) {
+      throw error; // Rethrow the error so useQuery can handle it
+    }
+  };
 
+  const [receipename, setReceipename] = useState("");
 
-const[receipename,setReceipename] = useState('');
-
-  const { register, handleSubmit, formState: { errors } } = useForm<Types>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Types>();
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["getRecipes"],
@@ -48,15 +44,15 @@ const[receipename,setReceipename] = useState('');
   });
 
   const onSubmit = (data: Types) => {
-
-    setReceipename(data.Receipename)
-  }
-
-
-  
+    setReceipename(data.Receipename);
+  };
 
   if (isLoading) {
-    return <div className="text-2xl font-medium animate-bounce">Loading.....</div>;
+    return (
+      <div className="text-2xl font-medium animate-bounce flex flex-col justify-center items-center mt-12">
+        <ThreeCircles color="#7f8c8d" />
+      </div>
+    );
   }
 
   if (error) {
@@ -64,24 +60,39 @@ const[receipename,setReceipename] = useState('');
   }
 
   return (
-    <div className=" bg-red-200">
+    <div className="p-12 py-12">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Search for a recipe" {...register("Receipename", { required: true })} />
-        {errors.Receipename && <p className="text-red-500">This field is required</p>}
+        <input
+          type="text"
+          placeholder="Search for a recipe"
+          {...register("Receipename", { required: true })}
+        />
+        {errors.Receipename && (
+          <p className="text-red-500">This field is required</p>
+        )}
+
+        <div className="px-12 py-5">
+          {Array.isArray(data) &&
+            data?.map((recipe) => (
+<div key={recipe?.idMeal} className="flex flex-col justify-center items-center gap-10" >
+                <div>{recipe?.strMeal}</div>
+                <img
+                  src={recipe?.strMealThumb}
+                  alt={recipe?.strMeal}
+                  className="h-[220px] w-[220px] md:h-[300px] md:w-[300px]"
+                />
+                
+                <p>Ingredients</p>
+                <p>{recipe?.strInstructions}</p>
+              </div>
+
         
-        <div>
-          {Array.isArray(data) && data?.map((recipe) => (
-            <div key={recipe?.idMeal}>
-              <div>{recipe?.strMeal}</div>
-              <img src={recipe?.strMealThumb} alt={recipe?.strMeal} className="h-20 w-20 md:w-10" />
-              <p>Ingredients</p>
-              <p>{recipe?.strInstructions}</p>
-            </div>
-          ))}
+              
+            ))}
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default Searchbar;
